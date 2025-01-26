f71340accd9b4f73fe9fd323f813aab35ec52837
Nishant Malik <nishant916@gmail.com>
2025-01-26 15:19:36 +0100

    Added validation to some of the fields of cours-settings page.

--- src/main/java/com/classroom/tracker/TrackerApplication.java
+++ src/main/java/com/classroom/tracker/TrackerApplication.java
@@ -8,12 +8,11 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class TrackerApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerApplication.class, args);
 	}
 
-	// Calling loadData() manually when the application starts
+	// Calling loadData() when the application starts
 	@Bean
 	public CommandLineRunner loadData(DataLoader dataLoader) {
 		return args -> dataLoader.loadData();
